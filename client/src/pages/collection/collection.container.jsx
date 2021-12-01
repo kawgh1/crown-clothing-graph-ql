c2f885aecca67b551c970b67855acfe515dd96b2
kawgh1 <krseven@gmail.com>
2021-12-01 09:41:47 -0600

    added node server to get around CORS issue for fetching image and DB resources

--- client/src/pages/collection/collection.container.jsx
+++ client/src/pages/collection/collection.container.jsx
@@ -27,7 +27,7 @@ const CollectionPageContainer = ({ match }) => (
     >
         {({ loading, data }) => {
             if (loading) return <Spinner />;
-            const { getCollectionsByTitle } = data || {};
+            const { getCollectionsByTitle } = data;
             console.log("data= ", data);
             return <CollectionPage collection={getCollectionsByTitle} />;
         }}
