56bbd9d75513d3493a92eac10e33d327da122e7c
kawgh1 <krseven@gmail.com>
2021-12-01 08:30:10 -0600

    heroku deploy

--- src/pages/collection/collection.container.jsx
+++ src/pages/collection/collection.container.jsx
@@ -25,8 +25,9 @@ const CollectionPageContainer = ({ match }) => (
         query={GET_COLLECTION_BY_TITLE}
         variables={{ title: match.params.collectionId }}
     >
-        {({ loading, data: { getCollectionsByTitle } }) => {
+        {({ loading, data }) => {
             if (loading) return <Spinner />;
+            const { getCollectionsByTitle } = data;
             return <CollectionPage collection={getCollectionsByTitle} />;
         }}
     </Query>
